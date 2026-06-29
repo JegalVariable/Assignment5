c5586e8964445eb6aee0b363d7a94f2979a977fb
variable <jegalvariable@gmail.com>
2026-06-29 17:05:37 +0900

    IMC 구현

--- Source/Assignment5/Private/HSCharacter.cpp
+++ Source/Assignment5/Private/HSCharacter.cpp
@@ -1,6 +1,9 @@
 #include "HSCharacter.h"
+#include  "HSPlayerController.h"
+#include "EnhancedInputComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AHSCharacter::AHSCharacter()
 {
@@ -15,6 +18,11 @@ AHSCharacter::AHSCharacter()
 	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 	CameraComp->bUsePawnControlRotation = false;
 
+	NormalSpeed = 600.0f;
+	SprintSpeedMultiplier = 1.5f;
+	SprintSpeed = NormalSpeed * SprintSpeedMultiplier;
+
+	GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
 }
 
 void AHSCharacter::BeginPlay()
@@ -32,6 +40,128 @@ void AHSCharacter::Tick(float DeltaTime)
 void AHSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        // IA를 가져오기 위해 현재 소유 중인 Controller를 ASpartaPlayerController로 캐스팅
+        if (AHSPlayerController* PlayerController = Cast<AHSPlayerController>(GetController()))
+        {
+            if (PlayerController->MoveAction)
+            {
+                // IA_Move 액션 키를 "키를 누르고 있는 동안" Move() 호출
+                EnhancedInput->BindAction(
+                    PlayerController->MoveAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AHSCharacter::Move
+                );
+            }
+            
+            if (PlayerController->JumpAction)
+            {
+                // IA_Jump 액션 키를 "키를 누르고 있는 동안" StartJump() 호출
+                EnhancedInput->BindAction(
+                    PlayerController->JumpAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AHSCharacter::StartJump
+                );
+                
+                // IA_Jump 액션 키에서 "손을 뗀 순간" StopJump() 호출
+                EnhancedInput->BindAction(
+                    PlayerController->JumpAction,
+                    ETriggerEvent::Completed,
+                    this,
+                    &AHSCharacter::StopJump
+                );
+            }
+            
+            if (PlayerController->LookAction)
+            {
+                // IA_Look 액션 마우스가 "움직일 때" Look() 호출
+                EnhancedInput->BindAction(
+                    PlayerController->LookAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AHSCharacter::Look
+                );
+            }
+            
+            if (PlayerController->SprintAction)
+            {
+                // IA_Sprint 액션 키를 "누르고 있는 동안" StartSprint() 호출
+                EnhancedInput->BindAction(
+                    PlayerController->SprintAction,
+                    ETriggerEvent::Triggered, 
+                    this, 
+                    &AHSCharacter::StartSprint
+                );
+                // IA_Sprint 액션 키에서 "손을 뗀 순간" StopSprint() 호출
+                EnhancedInput->BindAction(
+                    PlayerController->SprintAction, 
+                    ETriggerEvent::Completed, 
+                    this, 
+                    &AHSCharacter::StopSprint
+                );
+            }    
+        }
+    }
 }
 
+
+void AHSCharacter::Move(const FInputActionValue& value)
+{
+	if (!Controller) return;
+	
+	const FVector2D MoveInput = value.Get<FVector2D>();
+
+	if (!FMath::IsNearlyZero(MoveInput.X))
+	{
+		AddMovementInput(GetActorForwardVector(), MoveInput.X);
+	}
+
+	if (!FMath::IsNearlyZero(MoveInput.Y))
+	{
+		AddMovementInput(GetActorRightVector(), MoveInput.Y);
+	}
+}
+
+void AHSCharacter::StartJump(const FInputActionValue& value)
+{
+	if (value.Get<bool>())
+	{
+		Jump();
+	}
+}
+
+void AHSCharacter::StopJump(const FInputActionValue& value)
+{
+	if (!value.Get<bool>())
+	{
+		StopJumping();
+	}
+}
+
+void AHSCharacter::Look(const FInputActionValue& value)
+{
+	FVector2D LookInput = value.Get<FVector2D>();
+	
+	AddControllerYawInput(LookInput.X);
+	AddControllerPitchInput(LookInput.Y);
+}
+
+void AHSCharacter::StartSprint(const FInputActionValue& value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+	}
+}
+
+void AHSCharacter::StopSprint(const FInputActionValue& value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+	}
+}
--- Source/Assignment5/Public/HSCharacter.h
+++ Source/Assignment5/Public/HSCharacter.h
@@ -6,6 +6,7 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+struct FInputActionValue;
 
 UCLASS()
 class ASSIGNMENT5_API AHSCharacter : public ACharacter
@@ -15,14 +16,35 @@ class ASSIGNMENT5_API AHSCharacter : public ACharacter
 public:
 	AHSCharacter();
 	
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
-protected:
-	virtual void BeginPlay() override;
-	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	USpringArmComponent* SpringArmComp;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	UCameraComponent* CameraComp;
+	
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float NormalSpeed; // 기본 걷기 속도
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float SprintSpeedMultiplier;  // "기본 속도" 대비 몇 배로 빠르게 달릴지 결정
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float SprintSpeed; 	// 실제 스프린트 속도
+	
+	virtual void BeginPlay() override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UFUNCTION()
+	void Move(const FInputActionValue& value);
+	UFUNCTION()
+	void StartJump(const FInputActionValue& value);
+	UFUNCTION()
+	void StopJump(const FInputActionValue& value);
+	UFUNCTION()
+	void Look(const FInputActionValue& value);
+	UFUNCTION()
+	void StartSprint(const FInputActionValue& value);
+	UFUNCTION()
+	void StopSprint(const FInputActionValue& value);
 };
