c2ea1a233310b3e5bb5bb0556b0b51a74498e825
variable <jegalvariable@gmail.com>
2026-06-30 01:35:33 +0900

    DataTable 적용

--- Source/Assignment5/Public/SpawnVolume.h
+++ Source/Assignment5/Public/SpawnVolume.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ItemSpawnRow.h" 
 #include "SpawnVolume.generated.h"
 
 class UBoxComponent;
@@ -14,16 +15,21 @@ class ASSIGNMENT5_API ASpawnVolume : public AActor
 public:	
 	ASpawnVolume();
 	
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void SpawnRandomItem();
+	
+protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawning")
 	USceneComponent* Scene;
 	// 스폰 영역을 담당할 박스 컴포넌트
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawning")
 	UBoxComponent* SpawningBox;
-
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	UDataTable* ItemDataTable;
+	
 	// 스폰 볼륨 내부에서 무작위 좌표를 얻어오는 함수
-	UFUNCTION(BlueprintCallable, Category="Spawning")
 	FVector GetRandomPointInVolume() const;
+	FItemSpawnRow* GetRandomItem() const;
 	// 특정 아이템 클래스를 스폰하는 함수
-	UFUNCTION(BlueprintCallable, Category="Spawning")
 	void SpawnItem(TSubclassOf<AActor> ItemClass);
 };
