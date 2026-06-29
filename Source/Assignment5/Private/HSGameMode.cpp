1495a3750c036a0724ffb5ab82cdd88522003d41
variable <jegalvariable@gmail.com>
2026-06-29 16:12:42 +0900

    컨트롤러 생성

--- Source/Assignment5/Private/HSGameMode.cpp
+++ Source/Assignment5/Private/HSGameMode.cpp
@@ -1,7 +1,9 @@
 #include "HSGameMode.h"
 #include "HSCharacter.h"
+#include "HSPlayerController.h"
 
 AHSGameMode::AHSGameMode()
 {
 	DefaultPawnClass = AHSCharacter::StaticClass();
+	PlayerControllerClass = AHSPlayerController::StaticClass();
 }
---
+++ Source/Assignment5/Public/HSPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HSPlayerController.generated.h"
+
+UCLASS()
+class ASSIGNMENT5_API AHSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
